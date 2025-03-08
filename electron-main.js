f197375abaf04d6cb0b02b77c9337c20a1b6ff1d
Chris <ChrisDBarnett@live.com>
2025-03-07 23:00:19 -0700

    Transition to Compiler
    Transitioning the game from a browser based to a .exe program.

---
+++ electron-main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Create the browser window.
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  // Load the index.html file
+  mainWindow.loadFile('index.html');
+
+  // Open DevTools (uncomment during development if needed)
+  // mainWindow.webContents.openDevTools();
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window when the dock icon is clicked
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Quit when all windows are closed, except on macOS.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
